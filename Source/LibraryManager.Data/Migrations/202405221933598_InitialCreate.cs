4d26045dd24effb55057b4a2b43ba346d6ed06e3
Iowkey <n.vlasenk.o.envy@gmail.com>
2024-05-22 22:42:13 +0300

    Added migrations

---
+++ Source/LibraryManager.Data/Migrations/202405221933598_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿namespace LibraryManager.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        BookId = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 255),
+                        Author = c.String(nullable: false, maxLength: 255),
+                        ISBN = c.String(nullable: false, maxLength: 13),
+                        PublicationYear = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        CategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.BookId)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 100),
+                        Description = c.String(nullable: false, maxLength: 500),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Books", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Books", new[] { "CategoryId" });
+            DropTable("dbo.Categories");
+            DropTable("dbo.Books");
+        }
+    }
+}
---
+++ Source/LibraryManager.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace LibraryManager.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LibraryManager.Data.DataContext.LibraryContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LibraryManager.Data.DataContext.LibraryContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
